5d24bb004dfa567b3d0a7f10244fc57e6b56ca6b
jefri-netizen <jefriiimarbun@gmail.com>
2024-06-20 19:57:57 +0700

    update fetch untuk hide key dan error log versi 2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = ({ movies, loading, error }) => {
   return (
     <div className="container mt-4">
       {loading && <div className="text-center"><p>Loading...</p></div>}
-      {error && <div className="text-center"><p>Error: {error}</p></div>}
+      {error && <div className="text-center"><p>Silahkan masukkan Api Key pribadi anda terlebih dahulu : {error}</p></div>}
       
       {!loading && !error && movies.length === 0 && (
         <div className="text-center"><p>Movies not found</p></div>
